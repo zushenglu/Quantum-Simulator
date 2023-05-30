09af2bf14fa880dfdf7a61399068f61f0da0e04f
zushenglu <zushenglu1809@gmail.com>
2023-05-29 22:52:50 -0400

    implemented tensor product, init statevector simulator

--- run.c
+++ run.c
@@ -24,42 +24,43 @@ int main(int argnum, char** arg){
     printf("mx = %.1f%+.1fi\n", creal(qubit.x), cimag(qubit.x));
     printf("my = %.1f%+.1fi\n", creal(qubit.y), cimag(qubit.y));
 
-    int SIZE = 5;
+    int SIZE = 3;
 
     Circuit *qc = INIT_CIRCUIT(SIZE);
 
     // add parameterized single gate
-    RZ(qc,2,M_PI);
+    // RZ(qc,2,M_PI);
 
     // add regular single gate
     PauliX(qc,0);
 
     // // add unparameterized multi-gate
-    CX(qc,0,1);
+    // CX(qc,0,1);
     CX(qc,1,0);
 
-    RZ(qc,2,M_PI/4);
+    // RZ(qc,2,M_PI/4);
 
-    PauliX(qc,3);
+    // PauliX(qc,3);
     PRINT_CIRCUIT(qc,SIZE);
 
-    PRINT_QUBIT_STAT(qc->Q[3]);
+    // PRINT_QUBIT_STAT(qc->Q[3]);
 
-    float complex *arr = malloc(sizeof(float complex)*2);
-    Qubit* qbt = qc->Q[3];
-    arr[0] = qbt->x;
-    arr[1] = qbt->y;
+    // float complex *arr = malloc(sizeof(float complex)*2);
+    // Qubit* qbt = qc->Q[3];
+    // arr[0] = qbt->x;
+    // arr[1] = qbt->y;
 
-    Operation* op = qbt->next;
-    Gate* tg = op->gate;
+    // Operation* op = qbt->next;
+    // Gate* tg = op->gate;
 
-    arr = MX_MAP(arr,2,tg->mx, tg->dimension);
-    qbt->x = arr[0];
-    qbt->y = arr[1];
+    // arr = MX_MAP(arr,2,tg->mx, tg->dimension);
+    // qbt->x = arr[0];
+    // qbt->y = arr[1];
 
-    PRINT_QUBIT_STAT(qc->Q[3]);\
+    // PRINT_QUBIT_STAT(qc->Q[3]);
 
     
+    simulate(qc);
 
     // PRINT_MX(qc->Q[3]->next->gate->mx, qc->Q[3]->next->gate->dimension);
 
--- simulator.h
+++ simulator.h
@@ -2,8 +2,11 @@
 #define Qsimulator
 
 #include <complex.h>
+
+struct struct_circuit;
+
 float complex* MX_MAP(float complex * vector, int vec_dim, float complex ** mx, int mx_dim);
 
-
+void simulate(struct struct_circuit *circuit);
 
 #endif
