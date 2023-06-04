dd54fafed86637362f29be61f37a64d2f259f6a8
zushenglu <zushenglu1809@gmail.com>
2023-06-04 00:36:30 -0400

    init for package

---
+++ Q-sim.h
@@ -0,0 +1,13 @@
+#ifndef Q_Sim
+#define Q_Sim
+
+#include "qubit.h"
+#include "gate.h"
+#include "circuit.h"
+#include "simulator.h"
+#include <stdio.h>
+#include <complex.h>
+#include <math.h>
+#include <stdlib.h>
+
+#endif
